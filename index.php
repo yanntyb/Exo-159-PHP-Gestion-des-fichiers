3d74143e5e0fad4eed135d9356cb0beaeed8e954
Nux007 <007.nux@gmail.com>
2021-01-24 13:52:52 +0100

    Exo 159.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 1. Récupérer le contenu du fichier lire.txt à l'aide de la fonction file_get_contents
+ * 2. Afficher le contenu du fichier dans un div
+ */
+// TODO Votre code ici.
+
+/**
+ * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
+ * 4. Affichez le contenu de chaque ligne dans un nouveau span.
+ * --> N'oubliez pas de fermer votre fichier.
+ */
+// TODO Votre code ici.
+
+
+/**
+ * 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
+ * --> N'oubliez pas de fermer votre fichier.
+ */
+// TODO Votre code ici.
+
+
+/**
+ * 6. Ne cherchez pas la logique dans cet exo ^^
+ */
